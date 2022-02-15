cee0f64c4d4c3ba39ee0aecf51cb768970ae38d5
Tom Brown <herestomwiththeweather@gmail.com>
2022-02-15 16:14:47 -0600

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_15_203510) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_15_220554) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_15_203510) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
+  create_table "logs", force: :cascade do |t|
+    t.string "status"
+    t.string "note"
+    t.integer "page"
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_logs_on_book_id"
+    t.index ["user_id"], name: "index_logs_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "url"
@@ -33,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_15_203510) do
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "logs", "books"
+  add_foreign_key "logs", "users"
 end
